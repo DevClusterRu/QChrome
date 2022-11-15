5e3255545f5a93583673cc95516edd9d90977f29
Den <weblifeon@gmail.com>
2022-11-15 13:05:59 +0300

    last

--- internal/request.go
+++ internal/request.go
@@ -60,7 +60,7 @@ func Search(w http.ResponseWriter, req *http.Request) {
 		Browsers[br.Token] = br
 		go func(t string) {
 			//Браузер живет 120 сек и умирает
-			time.Sleep(5 * time.Minute)
+			time.Sleep(24 * time.Hour)
 			br.Close()
 			delete(Browsers, t)
 			log.Println("Browser is deleted")
