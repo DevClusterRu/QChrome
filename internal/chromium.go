444bb403cb96c53037f2a21311ba93cfb816cc11
Den <weblifeon@gmail.com>
2022-11-29 11:59:22 +0300

    last

--- internal/chromium.go
+++ internal/chromium.go
@@ -224,12 +224,12 @@ func (dp *Instance) FindNodes(sets []string) error {
 
 				if good && len(child.Children) > 0 {
 					if child.Children[0] != nil {
-						dp.Data[mapKey][key] = strings.Join(child.Children[0].Attributes, ",")
+						dp.Data[mapKey][key] = strings.Join(child.Attributes, ",")
+						dp.Data[mapKey][key] = dp.Data[mapKey][key] + "," + strings.Join(child.Children[0].Attributes, ",")
 						dp.Data[mapKey][key] = dp.Data[mapKey][key] + "[" + child.Children[0].NodeValue + "]"
 					}
 				}
 			}
-
 		}
 		return nil
 	}))
